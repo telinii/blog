fe45db407967c7ab8f24a137eac875c024d71857
Luca <gply714@gmail.com>
2026-08-12 23:51:07 -0300

    feat: adiciona express, tsx e teste de conexão com o banco

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
-const mensagem: string =  "Hello Blog!";
-console.log(mensagem);
+import "dotenv/config";
+import { PrismaClient } from "./generated/prisma/client";
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+let adapter: PrismaMariaDb; // declarei aqui, vazio por enquanto
+
+if (process.env.DATABASE_URL) {
+  adapter = new PrismaMariaDb(process.env.DATABASE_URL);
+}
+else {
+    throw new Error("DATABASE_URL não está definida no .env")
+}
+const  prisma = new PrismaClient({adapter});
+console.log("Conectado!")
